9c743dfdc1e19858d6e82cdf678f92ebcc6108c6
Kevin <kevin@byte2c.com>
2026-06-12 20:16:53 +0800

    debug login page

--- app/Views/league/login.php
+++ app/Views/league/login.php
@@ -3,6 +3,17 @@ declare(strict_types=1);
 
 /** @var array<int, string> $errors */
 /** @var array<string, string> $formData */
+
+// ... existing code ...
+
+// Temporarily add these lines for debugging:
+error_log('DEBUG: $_SERVER[\'SCRIPT_NAME\'] = ' . ($_SERVER['SCRIPT_NAME'] ?? 'NOT SET'));
+error_log('DEBUG: $_SERVER[\'REQUEST_URI\'] = ' . ($_SERVER['REQUEST_URI'] ?? 'NOT SET'));
+error_log('DEBUG: $appConfig[\'base_url\'] after derivation = ' . ($appConfig['base_url'] ?? 'NOT SET'));
+error_log('DEBUG: $routePath before dispatch = ' . $routePath);
+// End of temporary debug lines
+
+$router = new Router();
 ?>
 <section class="panel">
     <h1>Login to League</h1>
@@ -31,3 +42,4 @@ declare(strict_types=1);
         </div>
     </form>
 </section>
+
